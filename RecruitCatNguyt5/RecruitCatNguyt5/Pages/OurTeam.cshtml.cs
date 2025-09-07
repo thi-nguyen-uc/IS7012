247e255f5a1e3228fb64382152c6b3ec38a392ed
Thi Nguyen <nguyt5@mail.uc.edu>
2025-09-07 01:28:46 -0700

    initial commit

---
+++ RecruitCatNguyt5/RecruitCatNguyt5/Pages/OurTeam.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RecruitCatNguyt5.Pages
+{
+    public class OurTeamModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
