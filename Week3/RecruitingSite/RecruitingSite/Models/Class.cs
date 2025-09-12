08176578fb5ed32890f5f3d88ed384c45ae255d4
Thi Nguyen <nguyt5@mail.uc.edu>
2025-09-11 23:37:24 -0700

    added RecruitingSite project

---
+++ Week3/RecruitingSite/RecruitingSite/Models/Class.cs
@@ -0,0 +1,48 @@
+﻿namespace RecruitingSite.Models
+{
+    public class Candidate
+    {
+        public int CandidateId { get; set; } // PK
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Resume { get; set; }
+
+        // many-many: candidates-jobtitles
+        public List<JobTitle> JobTitles { get; set; }
+    }
+    public class Company
+    {
+        public int CompanyId { get; set; }  // PK
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public int IndustryId { get; set; } // FK
+        public Industry Industry { get; set; }
+
+        // one-many: company-jobtitles
+        public List<JobTitle> JobTitles { get; set; }
+     }
+
+    public class JobTitle
+    {
+        public int JobTitleId { get; set; } // PK
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        // one-many: company-jobtitles
+        public int CompanyId { get; set; } // FK
+        public Company Company { get; set; }
+
+        // many-many: jobtitles-candidates
+        public List<Candidate> Candidates { get; set; }
+    }
+    public class Industry
+    {
+        public int IndustryId { get; set; } // PK
+        public string Name { get; set; }
+
+        // one-many: industry-companies
+        public List<Company> Companies { get; set; }
+    }
+
+}
