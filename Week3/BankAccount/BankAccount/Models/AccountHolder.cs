648ac6aaaff4fe349915e4ddcc7699cc6df4babe
Thi Nguyen <nguyt5@mail.uc.edu>
2025-09-19 22:27:31 -0700

    renamed namespace, added display names

--- Week3/BankAccount/BankAccount/Models/AccountHolder.cs
+++ Week3/BankAccount/BankAccount/Models/AccountHolder.cs
@@ -1,14 +1,22 @@
-﻿namespace BankAccount.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BankAccount.Models
 {
     public class AccountHolder
     {
         public int AccountHolderId { get; set; }
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
+        [Display(Name = "Date of Birth")]
+        [DataType(DataType.Date)]
         public DateTime? DateOfBirth { get; set; }
         public string Email { get; set; }
+        [Display(Name = "Phone Number")]
         public string PhoneNumber { get; set; }
         public List<BankAccount>? BankAccounts { get; set; }
+        [Display(Name = "Full Name")]
         public string FullName => $"{FirstName} {LastName}";
     }
 }
--- Week3/BankAccount/BankAccount/Models/BankAccount.cs
+++ Week3/BankAccount/BankAccount/Models/BankAccount.cs
@@ -1,13 +1,19 @@
-﻿namespace BankAccount.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BankAccount.Models
 {
     public class BankAccount
     {
         public int BankAccountId { get; set; }
+        [Display(Name = "Account Number")]
         public string AccountNumber { get; set; }
+        [Display(Name = "Account Name")]
         public string AccountName { get; set; }
         public decimal Balance { get; set; }
+        [Display(Name = "Date Opened")]
         public DateTime DateOpened { get; set; }
         public int AccountHolderId { get; set; }
+        [Display(Name = "Account Holder")]
         public AccountHolder? AccountHolder { get; set; }
     }
     
